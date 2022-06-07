f9d481c26cdc46acca090d71101d4ba86fb40ee2
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2022-06-07 11:18:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        // recojo la instancia unica
+        Model miDB = Model.getInstance();
+        // utilizo un método de la instancia
+        String miNombre = miDB.getUserById(1);
+        Integer miScore = miDB.getScoreByUser(miNombre);
+        System.out.println(miNombre + " tiene: " + miScore + " puntos");
+    }
+}
